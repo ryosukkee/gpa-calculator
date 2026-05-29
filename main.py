604b642812cbe541e21de560faf927adfae19d5d
ryosukkee <animeryosuke@gmail.com>
2026-05-29 17:54:48 +0530

    some changes

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+lookup_table = [
+    (80, 10),
+    (70, 9),
+    (60, 8),
+    (50, 7),
+    (40, 6),
+    (30, 5),
+    (20, 4),
+    (10, 3),
+    (0, 2)
+]
+
 total_subjects = int(input("\nEnter the number of subjects: "))
 
 subject = []
@@ -7,102 +19,74 @@ marks = []
 gpa = []
 
 for i in range(total_subjects):
+
     subjectname = input("\n\nEnter the name of the subject: ")
     creditssubject = int(input("Enter the credits of the subject: "))
     ispractical = input("Subject is practical or not? (y/n): ")
 
     if ispractical == "y":
+
         labmarks = int(input("Enter lab marks of the subject: "))
         vivamarks = int(input("Enter viva marks of the subject: "))
 
         totalmarks = labmarks + vivamarks
 
-        if totalmarks >= 80:
-            lgpa = 10
-
-        elif totalmarks >= 70:
-            lgpa = 9
-
-        elif totalmarks >= 60:
-            lgpa = 8
-
-        elif totalmarks >= 50:
-            lgpa = 7
-
-        elif totalmarks >= 40:
-            lgpa = 6
-
-        elif totalmarks >= 30:
-            lgpa = 5
-
-        elif totalmarks >= 20:
-            lgpa = 4
-
-        elif totalmarks >= 10:
-            lgpa = 3
-
-        else:
-            lgpa = 2
+        for cutoff, grade in lookup_table:
+            if totalmarks >= cutoff:
+                lgpa = grade
+                break
 
         subject.append(subjectname)
         marks.append(totalmarks)
-        finalgpa = lgpa*creditssubject
+
+        finalgpa = lgpa * creditssubject
         gpa.append(finalgpa)
+
         maxmarks.append(100)
         maxcredits.append(creditssubject)
 
-        print(f"\nScored {lgpa}GPA is {subjectname} with {totalmarks} marks")
+        print(f"\nScored {lgpa} GPA in {subjectname} with {totalmarks} marks")
 
     else:
+
         internnalmarks = int(input("Enter the internals of the subject: "))
         midsemmarks = int(input("Enter the midsem of the subject: "))
         endsmarks = int(input("Enter the endsem of the subject: "))
 
-        cendsemmakrs = endsmarks/2
-    
-        totalmarks = internnalmarks + midsemmarks + cendsemmakrs
+        cendsemmarks = endsmarks / 2
 
-        if totalmarks >= 80:
-            lgpa = 10
+        totalmarks = internnalmarks + midsemmarks + cendsemmarks
 
-        elif totalmarks >= 70:
-            lgpa = 9
-
-        elif totalmarks >= 60:
-            lgpa = 8
-
-        elif totalmarks >= 50:
-            lgpa = 7
-
-        elif totalmarks >= 40:
-            lgpa = 6
-
-        elif totalmarks >= 30:
-            lgpa = 5
-
-        elif totalmarks >= 20:
-            lgpa = 4
-
-        elif totalmarks >= 10:
-            lgpa = 3
-
-        else:
-            lgpa = 2
+        for cutoff, grade in lookup_table:
+            if totalmarks >= cutoff:
+                lgpa = grade
+                break
 
         subject.append(subjectname)
         marks.append(totalmarks)
-        finalgpa = lgpa*creditssubject
+
+        finalgpa = lgpa * creditssubject
         gpa.append(finalgpa)
+
         maxmarks.append(100)
         maxcredits.append(creditssubject)
 
-        print(f"\nScored {lgpa}GPA is {subjectname} with {totalmarks} marks")
+        print(f"\nScored {lgpa} GPA in {subjectname} with {totalmarks} marks")
 
 totalmarksscored = sum(marks)
 totalgpascored = sum(gpa)
+
 maximummarks = sum(maxmarks)
 maximumcredits = sum(maxcredits)
 
-final = totalgpascored/maximumcredits
+final = totalgpascored / maximumcredits
 
-print(f"You scored {final}SGPA with {totalmarksscored}/{maximummarks} marks!")
+print("\n" + "=" * 40)
+print("SEMESTER RESULT")
+print("=" * 40)
+
+for i in range(len(subject)):
+    print(f"{subject[i]} : {marks[i]}/100")
+
+print(f"\nTotal Marks : {totalmarksscored}/{maximummarks}")
+print(f"SGPA        : {final:.2f}")
